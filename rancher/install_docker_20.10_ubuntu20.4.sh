e547c0ddc01259ae0cc213197370813f11a4ab50
Maxence Laurent <maxence.laurent@gmail.com>
2022-01-06 18:02:19 +0100

    Sudo please

--- rancher/install_docker_20.10_ubuntu20.4.sh
+++ rancher/install_docker_20.10_ubuntu20.4.sh
@@ -2,36 +2,35 @@
 
 if [ ! -f /swapfile ]; then
     # Give some swap space
-    fallocate -l 1G /swapfile
-    chmod 600 /swapfile
-    mkswap /swapfile
+    sudo fallocate -l 1G /swapfile
+    sudo chmod 600 /swapfile
+    sudo mkswap /swapfile
 
-    swapon /swapfile
-    echo /swapfile swap swap defaults 0 0 >> /etc/fstab
+    sudo swapon /swapfile
+    sudo sh -c "echo /swapfile swap swap defaults 0 0 >> /etc/fstab"
 
-    echo vm.swappiness=10 >> /etc/sysctl.conf
-    sysctl vm.swappiness=10
+    sudo sh -c "echo vm.swappiness=10 >> /etc/sysctl.conf"
+    sudo sysctl vm.swappiness=10
 fi
 
-DEBIAN_FRONTEND=noninteractive apt-get install -y augeas-tools
+DEBIAN_FRONTEND=noninteractive sudo apt-get install -y augeas-tools
 
 ## Rotate logs daily
 (echo "set \
 /files/etc/logrotate.d/*/rule[file='/var/log/btmp']/schedule daily"; \
-echo save) | augtool
+echo save) | sudo augtool
 
 (echo "set \
 /files/etc/logrotate.d/*/rule[file='/var/log/auth.log']/schedule daily"; \
-echo save) | augtool
+echo save) | sudo augtool
 
 ## Limit journald max size
 JOURNALD_CONF=/etc/systemd/journald.conf
 if [ -f ${JOURNALD_CONF} ]; then
-    echo "SystemMaxUse=100M" >> ${JOURNALD_CONF}
-    systemctl restart systemd-journald
+    sudo sh -c "echo SystemMaxUse=100M >> ${JOURNALD_CONF}"
+    sudo systemctl restart systemd-journald
 fi
 
 
 # Install docker
 curl https://releases.rancher.com/install-docker/20.10.sh | sh
-
